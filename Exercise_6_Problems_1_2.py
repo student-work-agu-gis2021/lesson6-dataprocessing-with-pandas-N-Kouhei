29d365338a34f47ce1eca8edad04484aaf3c8ccb
航平 <>
2021-05-23 14:56:52 +0000

    I finished Problem 2!

--- Exercise_6_Problems_1_2.py
+++ Exercise_6_Problems_1_2.py
@@ -17,6 +17,7 @@ import numpy as np
 data = None
 
 # YOUR CODE HERE 1
+# read data, skip second row contains ---, convert -9999 into NaN.
 fp="data/1091402.txt"
 data= pd.read_csv(fp,delim_whitespace=True,skiprows=[1],na_values=[-9999])
 
@@ -33,6 +34,7 @@ print(data.tail())
 
 tavg_nodata_count = None
 #YOUR CODE HERE 2
+# count number of NaN on "TAVG" by isnull() and sum()
 tavg_nodata_count = data["TAVG"].isnull().sum()
 
 #CAUTION!!! DON'T EDIT THIS PART START
@@ -46,6 +48,7 @@ print('Number of no-data values in column "TAVG":',tavg_nodata_count)
 
 tmin_nodata_count = None
 #YOUR CODE HERE 3
+# count number of NaN on "TMIN" by isnull() and sum()
 tmin_nodata_count = data["TMIN"].isnull().sum()
 
 
@@ -60,6 +63,7 @@ print('Number of no-data values in column "TMIN":', tmin_nodata_count)
 
 day_count = None 
 #YOUR CODE HERE 4
+# count days by length of data
 day_count = len(data)
 
 #CAUTION!!! DON'T EDIT THIS PART START
@@ -74,6 +78,7 @@ print("Number of days:", day_count)
 first_obs = None
  
 # YOUR CODE HERE 5
+# select data of first observation day
 first_obs = data.iloc[0,4]
 #CAUTION!!! DON'T EDIT THIS PART START
 # Print out the solution:
@@ -86,6 +91,7 @@ print('Date of the first observation:',first_obs)
 last_obs = None
 
 # YOUR CODE HERE 6
+# select data of last observation day
 last_obs = data.iloc[day_count-1,4]
 
 #CAUTION!!! DON'T EDIT THIS PART START
@@ -100,6 +106,7 @@ print('Date of the last observation:', last_obs)
 avg_temp = None
 
 # YOUR CODE HERE 7
+# find the average of "TAVG" column
 avg_temp = np.mean(data.iloc[:,6])
 
 #CAUTION!!! DON'T EDIT THIS PART START
@@ -114,6 +121,7 @@ print('Average temperature (F) for the whole dataset:', round(avg_temp, 2))
 avg_temp_1969 = None
 
 # YOUR CODE HERE 8
+# find the average in the range of 19690501 ~ 19690831
 date_que = data.query("19690501<=DATE<=19690831")
 avg_temp_1969 = np.mean(date_que.iloc[:,7])
 
@@ -129,7 +137,26 @@ print('Average temperature (F) for the Summer of 69:', round(avg_temp_1969, 2))
 monthly_data = None
 
 # YOUR CODE HERE 9
+def fahr_to_celsius(temp_fahrenheit):
+  convert_temp = (temp_fahrenheit-32)/1.8
+  return convert_temp
 
+m_d = pd.DataFrame()
+
+data["DATE_STR"] = data["DATE"].astype(str)
+data["YEAR_MONTH"] = data["DATE_STR"].str.slice(start=0, stop=6)
+grouped = data.groupby("YEAR_MONTH")
+
+mean_col = ["TAVG"]
+for key, group in grouped:
+  mean_values = group[mean_col].mean()
+  mean_values["YEAR_MONTH"] = key
+  m_d = m_d.append(mean_values,ignore_index=True)
+
+monthly_data = pd.DataFrame(columns=["temp_celsius"])
+monthly_data["temp_celsius"] = fahr_to_celsius(m_d["TAVG"])
+
+print(monthly_data)
 #CAUTION!!! DON'T EDIT THIS PART START
 # This test print should print the length of variable monthly_data
 print(len(monthly_data))
