b3e4813f8e9664cc54bc86398b13b6689ae68c79
航平 <>
2021-05-23 05:26:16 +0000

    Extracted a specific date into "date_que".

--- Exercise_6_Problems_1_2.py
+++ Exercise_6_Problems_1_2.py
@@ -114,7 +114,8 @@ print('Average temperature (F) for the whole dataset:', round(avg_temp, 2))
 avg_temp_1969 = None
 
 # YOUR CODE HERE 8
-avg_temp_1969 = np.mean(data.iloc[(1969051 <= data.iloc[:,4] <= 19690831),7])
+date_que = data.query("19690501<=DATE<=19690831")
+avg_temp_1969 = np.mean(date_que.iloc[:,7])
 
 #CAUTION!!! DON'T EDIT THIS PART START
 # This test print should print a number
